5276ac81c2c5db11d9457e4e0595abd93352fbd7
Jose Guevara <joserafaelguevara@gmail.com>
2015-11-17 15:44:12 +0100

    More example tests added

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,8 +1,44 @@
 class Word < ActiveRecord::Base
   ## CUSTOM METHODS
-  # OPTIMIZE Get a random word without loading all words into main memory 
+
   def self.random
-    all = Word.all
-    all[rand(all.size)]
+    # all = Word.all
+    # all[rand(all.size)]
+    Word.offset(rand(Word.count)).take
+  end
+
+  # OPTIMIZE: Pick up a random translated word.
+  # Don't use Word.order("RAND()").take it hurts your DB performance
+  #
+  def self.random_translated_word
+    Word.all.select {|word|word.translated?}.sort_by {rand}[0]
+
+    # Hint: Generate a random number as an index offset.
+    # Index values must span [0,n-1]; n=table_size,
+    # then pick up the first record.
+    #
+    # Word.where.not(spanish: nil).offset(rand(Word.count)).take
+  end
+
+  def self.add word, translation
+    unless word.blank? || translation.blank?
+      if Word.find_by(english: word).nil?
+        Word.new(english: word, spanish: translation).save
+      end
+    end
+  end
+
+  def self.add_content content
+    if not content.empty?
+      content.split(" ").each do |english_word|
+        if Word.find_by(english: english_word).nil?
+          Word.new(english: english_word).save
+        end
+      end
+    end
+  end
+
+  def translated?
+    english and spanish
   end
 end
--- test/models/word_test.rb
+++ test/models/word_test.rb
@@ -18,4 +18,71 @@ class WordTest < ActiveSupport::TestCase
     # in random array quite high
     assert results.include?('yes')
   end
+
+  # Add a new word in the dictionary test
+  def test_count_after_add
+    count = Word.count
+    new_word = 'life'
+    new_translation = 'vida'
+
+    if not Word.exists?(english: new_word)
+      Word.add(new_word, new_translation)
+      assert_equal count + 1, Word.count
+    else
+      assert false, "\'#{new_word}\' ya está definida test/fixtures/words.rb, new_word debe ser una palabra nueva"
+    end
+  end
+
+  # Repeated words are not allowed for add method
+  def test_count_after_add_repeated_word
+    count = Word.count
+
+    # Add a new word
+    Word.add('moon', 'luna')
+    count_after = Word.count
+    assert_equal count + 1, count_after
+
+    # Try to add an already added word
+    Word.add('moon', 'luna')
+    assert_equal count_after, Word.count
+  end
+
+  # Empty words are not allowed in any form (nil, '', ' ') for add method
+  def test_count_after_add_nil_word
+    count = Word.count
+    Word.add(nil, nil)
+    Word.add(nil, 'nulo')
+    Word.add('example', nil)
+    assert_equal count, Word.count
+  end
+
+  def assert_count_after_add(count, content)
+    Word.add_content(content)
+    assert_equal count, Word.count
+  end
+
+  # Repeteated words are not allowed for add_content method
+  def test_add_content
+    count = Word.count
+    assert_count_after_add count, ""
+    assert_count_after_add count + 1, "single"
+    assert_count_after_add count + 3, "two words"
+    assert_count_after_add count + 7, "four elements are funny"
+    assert_count_after_add count + 8, "duplicate duplicate"
+  end
+
+  def test_translated
+    assert !Word.new.translated?
+    assert (Word.new english: 'yes', spanish: 'sí').translated?
+    assert !(Word.new spanish: 'sí').translated?
+    assert !(Word.new english: 'yes').translated?
+  end
+
+  def test_use_words_with_translated_word
+    Word.new(english: 'yes', spanish: 'sí').save
+    Word.add_content 'untranslated'
+    randoms = []
+    100.times {randoms << Word.random_translated_word.english}
+    assert_not randoms.include? 'untranslated'
+  end
 end
